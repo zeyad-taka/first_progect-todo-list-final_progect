096b170e6455a0275b36d72a19c20fa092de4141
Zeyad-taka <zeyadibrahim184@gmail.com>
2025-12-25 12:52:24 +0200

    uploud

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
